c8c09952a6a89e0378e3c616edf897b742f730ee
Peter Brennan <peter.brennan@fbd.ie>
2016-05-02 19:56:31 +0100

    Added calls
    added calls to Azure besides localhost

--- Program.cs
+++ Program.cs
@@ -5,6 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using RefuseCollect;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
 
 
 
@@ -18,7 +29,13 @@ namespace RefuseCollectClient
         {
             string baseAddress = "http://localhost:45055/";
             String guid = Guid.NewGuid().ToString();
+            String guid2 = Guid.NewGuid().ToString();
+            String guid3 = Guid.NewGuid().ToString();
+            string baseAddress2 = "http://pbrefusecollect.azurewebsites.net/";
+            string baseAddress3 = "http://pbrefusecollect.azurewebsites.net/api/values/";
             Console.WriteLine("This RowKey code is: "+guid);
+            Console.WriteLine("This RowKey code is: " + guid2);
+
             try
             {
                 HttpClient client6 = new HttpClient();
@@ -82,16 +99,66 @@ namespace RefuseCollectClient
 
             try
             {
+                HttpClient client2 = new HttpClient();
+                //string id, string pareaid, string platitude, string plongitude
+
+                //api / values
+
+                var postrefuse = new PostRefuse() { id = "D8", pareaid = guid2, platitude = "6", plongitude = "56" };
+
+                string postBody = JsonConvert.SerializeObject(postrefuse);
+                Console.WriteLine(postBody);
+                
+                var cli = new WebClient();
+                cli.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var response = cli.UploadString("http://localhost:45055/api/values/", postBody);
+
+
+                //string baseAddress2 = "http://localhost:45055/api/values/";
+
+                //var response2 = client2.PostAsJsonAsync("http://localhost:45055/api/values/", postrefuse).Result;
+                client2.Dispose();
+                Console.WriteLine(response);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            try
+            {
                 var putrefuse = new RefuseCollect.Controllers.PutRefuse() { id = "D8", pareaid = guid };
                 Console.WriteLine(putrefuse.id);
                 Console.WriteLine(putrefuse.pareaid);
 
                 HttpClient client3 = new HttpClient();
 
-                var response3 = client3.PutAsJsonAsync("http://localhost:45055/api/values", putrefuse).Result;
+                var response3 = client3.PutAsJsonAsync("http://localhost:45055/api/values", putrefuse).Result; // see https://social.msdn.microsoft.com/Forums/vstudio/en-US/42de08af-9563-4ad3-ad51-e909994c78f9/webclient-put-to-webapi-method?forum=csharpgeneral
                 client3.Dispose();
                 Console.WriteLine(response3);
                 client3.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            try
+            {
+                var putrefuse = new PutRefuse() { id = "D8", pareaid = guid2 };
+                Console.WriteLine(putrefuse.id);
+                Console.WriteLine(putrefuse.pareaid);
+
+                
+
+                string putBody = JsonConvert.SerializeObject(putrefuse);
+                Console.WriteLine(putBody);
+
+                var cli = new WebClient();
+                cli.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var response = cli.UploadString("http://localhost:45055/api/values/","PUT",putBody);
+                Console.WriteLine(response);
+
             }
             catch (Exception e)
             {
@@ -111,7 +178,108 @@ namespace RefuseCollectClient
                 Console.WriteLine(e.ToString());
             }
 
+            try
+            {
+                HttpClient client7 = new HttpClient();
+                //     if (Aggtype == "Agg" && Aggquery == "CountById")
+                // public int Get(String id, String Aggtype, String Aggquery)
+                // test calling the Azure website
+                var response2x = client7.GetAsync(baseAddress2 + "api/values/D8/Agg/CountById").Result;
 
+                Console.WriteLine(response2x);
+
+                Console.WriteLine("The count of records in D8 (calling from Azure) is: " + response2x.Content.ReadAsStringAsync().Result);
+
+                client7.Dispose();
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            try
+            {
+                HttpClient client6 = new HttpClient();
+                //http://localhost:45055/swagger
+                var response2x = client6.GetAsync(baseAddress2 + "api/values/D8/1qw11123234529064ef").Result;
+
+                Console.WriteLine(response2x);
+
+                Console.WriteLine("Calling the "+response2x.Content.ReadAsStringAsync().Result);
+
+                client6.Dispose();
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            try
+            {
+                HttpClient client2 = new HttpClient();
+                //
+                var postrefuse = new PostRefuse() { id = "D9", pareaid = guid3, platitude = "6", plongitude = "56" };
+
+                string postBody = JsonConvert.SerializeObject(postrefuse);
+                Console.WriteLine(postBody);
+
+                var cli = new WebClient();
+                cli.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var response = cli.UploadString(baseAddress3, postBody);
+                    
+                Console.WriteLine(response);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            try
+            {
+                var putrefuse = new PutRefuse() { id = "D9", pareaid = guid3 };
+                Console.WriteLine(putrefuse.id);
+                Console.WriteLine(putrefuse.pareaid);
+
+                string putBody = JsonConvert.SerializeObject(putrefuse);
+                Console.WriteLine(putBody);
+
+                var cli = new WebClient();
+                cli.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var response = cli.UploadString(baseAddress3, "PUT", putBody); // see https://social.msdn.microsoft.com/Forums/vstudio/en-US/42de08af-9563-4ad3-ad51-e909994c78f9/webclient-put-to-webapi-method?forum=csharpgeneral
+                Console.WriteLine("put request to azure webservice: "+response);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
+    }
+
+
+
+
+    internal class PutRefuse
+    {
+        public PutRefuse()
+        {
+        }
+
+        public string id { get; set; }
+        public string pareaid { get; set; }
+    }
+
+    internal class PostRefuse
+    {
+        public string id { get; set; }
+        public string pareaid { get; set; }
+
+        public string platitude { get; set; }
+
+        public string plongitude { get; set; }
     }
 }
